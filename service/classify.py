1b08d28788296a91e1c0ad6a40d65cab8773ab58
Peiran Yao <xavieryao@live.cn>
2018-11-29 15:51:30 +0800

    cache model

--- service/classify.py
+++ service/classify.py
@@ -1,11 +1,14 @@
 import service_impl.classify_impl
 
+classify_model_cache = {}
 
 def classify(text, name):
-    loader = service_impl.classify_impl.ClassifyLoader(
-        name=name,
-        pre_emb='models/glove/mixed_vec')
-    model, d_word_index = loader.load_stat('models/' + name + '/classify.ml')
+    if name not in classify_model_cache:
+        loader = service_impl.classify_impl.ClassifyLoader(
+            name=name,
+            pre_emb='models/glove/mixed_vec')
+        classify_model_cache[name] = loader.load_stat('models/' + name + '/classify.ml')
+    model, d_word_index = classify_model_cache[name]
     return service_impl.classify_impl.classify(text, model, d_word_index)
 
 
--- service/ner.py
+++ service/ner.py
@@ -1,10 +1,13 @@
 import service_impl.ner_impl
 
+ner_model_cache = {}
 
 def ner(text, name):
-    loader = service_impl.ner_impl.NERLoader(
-        ner_stat='models/' + name + '/ner_mapping.pkl',
-        ner_train='dataset/' + name + '/ner/train.dat',
-        pre_emb='models/glove/mixed_vec')
-    model, word_to_id, char_to_id, tag_to_id, id_to_tag = loader.load_stat('models/' + name + '/ner.ml')
+    if name not in ner_model_cache:
+        loader = service_impl.ner_impl.NERLoader(
+            ner_stat='models/' + name + '/ner_mapping.pkl',
+            ner_train='dataset/' + name + '/ner/train.dat',
+            pre_emb='models/glove/mixed_vec')
+        ner_model_cache[name] = loader.load_stat('models/' + name + '/ner.ml')
+    model, word_to_id, char_to_id, tag_to_id, id_to_tag = ner_model_cache[name]
     return service_impl.ner_impl.ner(text, model, word_to_id, char_to_id, tag_to_id, id_to_tag)
